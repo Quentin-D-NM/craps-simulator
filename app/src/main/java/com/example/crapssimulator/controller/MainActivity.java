949d5cf0a0c846c0b3515a1a7ba9a7122df41999
Quentin-D-NM <qdye@cnm.edu>
2019-09-24 16:57:55 -0600

    Added Dice Images.

--- app/src/main/java/com/example/crapssimulator/controller/MainActivity.java
+++ app/src/main/java/com/example/crapssimulator/controller/MainActivity.java
@@ -1,13 +1,14 @@
-package com.example.crapssimulator;
+package com.example.crapssimulator.controller;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.crapssimulator.R;
+import com.example.crapssimulator.view.RoundAdapter;
 import edu.cnm.deepdive.craps.model.Game;
 import edu.cnm.deepdive.craps.model.Game.Roll;
 import edu.cnm.deepdive.craps.model.Game.Round;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
   private Game game;
   private Random rng;
-  private ArrayAdapter<Roll> adapter;
+  private RoundAdapter adapter;
   private TextView tally;
   private ListView rolls;
 
@@ -27,9 +28,8 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     tally = findViewById(R.id.tally);
-    tally.setText("This will be my win/loss tally line");
     rolls = findViewById(R.id.rolls);
-    adapter = new ArrayAdapter<>(this, R.layout.single_roll);
+    adapter = new RoundAdapter(this);
     rolls.setAdapter(adapter);
     rng = new Random();
     resetGame();
@@ -58,10 +58,7 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void updateDisplay(Round round) {
-    adapter.clear();
-    if (round != null) {
-      adapter.addAll(round.getRolls());
-    }
+    adapter.add(round);
     tally.setText(getString(R.string.tally_format, game.getWins(),
         game.getPlays(), 100 * game.getPercentage()));
   }
